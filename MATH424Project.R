202379436a1e386c36b40326ff99d4603b23f717
Joseph Hunt <jhunt5813@gmail.com>
2017-12-10 13:11:08 -0700

    Final writeup, including all images.

--- MATH424Project.R
+++ MATH424Project.R
@@ -1,18 +1,29 @@
 pairs(swiss)
-baseModel = lm(swiss$Fertility + swiss$Agriculture + swiss$Examination + swiss$Education + swiss$Catholic + swiss$Infant.Mortality)
+baseModel = lm(swiss$Fertility ~ swiss$Agriculture + swiss$Examination + swiss$Education + swiss$Catholic + swiss$Infant.Mortality)
 summary(baseModel)
+hetBaseModel=lm(swiss$Fertility ~ swiss$Catholic)
 
-logExamModel = lm(swiss$Fertility + swiss$Agriculture + log(swiss$Examination) + swiss$Education + swiss$Catholic + swiss$Infant.Mortality)
+plot(fitted(hetBaseModel),resid(hetBaseModel))
+removedBaseModel = lm(swiss$Fertility ~ swiss$Education + swiss$Catholic + swiss$Infant.Mortality)
+summary(removedBaseModel)
+
+hetModel = lm(swiss$Fertility ~ swiss$Infant.Mortality)
+#summary(hetModel)
+plot(fitted(hetModel),resid(hetModel))
+
+logExamModel = lm(swiss$Fertility ~ swiss$Agriculture + log(swiss$Examination) + swiss$Education + swiss$Catholic + swiss$Infant.Mortality)
 summary(logExamModel)
 
-ExamDenomModel = lm(swiss$Fertility + swiss$Agriculture + 1/(swiss$Examination) + swiss$Education + swiss$Catholic + swiss$Infant.Mortality)
+dumDenomExam = 1/(swiss$Examination)
+
+ExamDenomModel = lm(swiss$Fertility ~ swiss$Agriculture + dumDenomExam + swiss$Education + swiss$Catholic + swiss$Infant.Mortality)
 summary(ExamDenomModel)
 
 plot(1/(swiss$Examination), swiss$Fertility)
 pairs(swiss)
 resid(ExamDenomModel)
 plot(fitted(ExamDenomModel),resid(ExamDenomModel))
-
+plot(fitted(baseModel),resid(baseModel))
 #Note: Try the base model excluding the swiss$Examination variable to see how it reacts.
 
 dummy = swiss
@@ -34,9 +45,12 @@ for(i in 1:47) {
 }
 dummy$Catholic = x
 
-EndModel = lm(dummy$Fertility ~ dummy$Agriculture + 1/dummy$Examination + dummy$Education + dummy$Catholic + dummy$Infant.Mortality )
+EndModel = lm(dummy$Fertility ~ dummy$Agriculture + dummy$Education + dummy$Catholic + dummy$Infant.Mortality )
+summary(EndModel)
+plot()
 #Decided this was the best music
-
+hetEndModel=lm(dummy$Fertility ~ dummy$Catholic)
+plot(fitted(hetEndModel),resid(hetEndModel))
 removedOutliers = head(swiss, 44)
 
 RemovedExamDenomModel = lm(removedOutliers$Fertility ~ removedOutliers$Agriculture + 1/(removedOutliers$Examination) + removedOutliers$Education + removedOutliers$Catholic + removedOutliers$Infant.Mortality)
